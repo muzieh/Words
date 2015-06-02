1bc175bf5d429b860a6bf554b9d03375c4b6fa8b
Marcin Owerczuk <muzieh@gmail.com>
2015-06-02 17:05:04 +0100

    kick off

---
+++ DataAccess/BloggingContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+	public class BloggingContext : DbContext, IBloggingContext
+	{
+		public DbSet<Blog> Blogs { get; set; }
+		public DbSet<Post> Posts { get; set; }
+	}
+}
